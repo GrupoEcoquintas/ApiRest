2f2421abc382bca6662ae68bc0bfbf619c1038d9
Luis Roberto <admin@ecoquintas.com>
2023-02-23 10:35:59 -0600

    Se crea el primer get a modo de prueba y se inicia el servidor con el listening

--- src/index.js
+++ src/index.js
@@ -7,3 +7,27 @@ const app = express();
 
 // La const morgan nos permite hacer uso del framework morgan
 const morgan = require('morgan');
+
+//Configuraciones para solicitudes y respuestas
+app.set('port', process.env.PORT || 3000);
+//app.set('json spaces', 2);
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+// Codificar los servicios que se expoen a modo de prueba
+app.get('/', (req, res) => {
+    res.json (
+        {
+            "Title": 'Hola Ecoquintas'
+        }
+    );
+})
+
+// Condificamos el "escucha" en el puerto definido anteriormente
+app.listen(app.get('port'), () => {
+    console.log(` Server Listening on port ${ app.get('port') }`);
+});
