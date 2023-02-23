bb484504fb2165d01544eae3e157e418256e44bc
Luis Roberto <admin@ecoquintas.com>
2023-02-23 09:41:03 -0600

    Se corre npm init, se instala express morgan y nodemon, tambien se hacen los requiere

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+// Inicializamos la const express nos permite hacer uso del framework express
+const express = require('express');
+
+// Asignamos la const express a la constante app
+const app = express();
+
+// La const morgan nos permite hacer uso del framework morgan
+const morgan = require('morgan');
