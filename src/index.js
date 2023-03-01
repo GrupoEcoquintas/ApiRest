080e23aed6c1a4dc08474e134d470dd823552ef6
Luis R <luiscr1104@gmail.com>
2023-02-28 22:01:01 -0600

    Agrego encabezados de seguridad. Recibo una respuesta con solo los datos relevantes. Tambien se instalan y requieren los cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 // Inicializamos la const express nos permite hacer uso del framework express
 const express = require('express');
 
+const cors = require('cors');
+
 // Asignamos la const express a la constante app
 const app = express();
 
@@ -13,34 +15,14 @@ const routes = require('./routes');
 const morgan = require('morgan');
 
 //Configuraciones para solicitudes y respuestas
-app.set('port', process.env.PORT || 3302);
+app.set('port', process.env.PORT || 3080);
 
 // Middleware
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
-
-// Codificar los servicios que se expone a modo de prueba
-// app.get('/', (req, res) => {
-//     connnectionAWSTest.query( 'SELECT * FROM tbl_users LIMIT 5', (err, results) => {
-//         if (err) throw err;
-//         //res.status(200).json ( results );
-//         res.json (
-//             {
-//                 results
-//             }
-//             );
-//             const data = Object.values(results);
-//             console.log(data)
-//     });
-// });
-
-
-//  Codificar los servicios que se expone con el llamado al procedimiento
-
-// Se define la funcion que llama al procedimiento que retorna si el cliente está activo
-
+app.use(cors());
 
 
 // Condificamos el "escucha" en el puerto definido anteriormente
--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,16 @@ const express = require('express');
 const router = express.Router();
 
 const mysql = require('mysql');
+const { restart } = require('nodemon');
 
 /* Llamo mysql para crear la conexion con los datos y configuracion del archivo config/config.js del servidor de DESARROLLO EN AWS */
 const connnectionAWS = mysql.createConnection(config.AWSDesarrolloEQ_DB);
 
-// // Manejo de Conexion o error 
-// connnectionAWSDesarrolloEQ.connect((err) => {
-//     if (err) throw (err);
-//     console.log('Successfully connected');
-// });
+// Manejo de Conexion o error 
+connnectionAWS.connect((err) => {
+    if (err) throw (err);
+    console.log('Successfully connected ✔️');
+});
 
 // Defino la rura de mi raiz
 router.get('/', (req, res) => {
@@ -26,14 +27,22 @@ router.get('/consulta-cliente/:cedula', (req, res) => {
     //Llama al procedimiento almacenado con la cedula especificada
     connnectionAWS.query('CALL PA_ConsultaClienteActivo(?)', [cedula], (err, results) => {
         if (err) {
-            return res.status(500).json({ message: 'Error al buscar el cliente' });
+            return res.status(500).json({ message: 'Error al buscar el cliente ✖️' });
         }
         if (results[0].length === 0) {
-            return res.status(404).json({ message: 'Cliente no encontrado' });
+            return res.status(404).json({ message: 'Cliente no encontrado ✖️' });
         }
 
+        // Agrega los encabezados
+        res.setHeader('Content-Type', 'application/json'); //para setear el tipo de archivo que recibimos
+        res.setHeader('Cache-Control', 'public, max-age=86400'); //para almacenar el caché un día 
+        res.setHeader('Access-Control-Allow-Origin', '*'); //permisos de acceso a la API
+        res.setHeader('ETag', '12345'); //identificador único para la respuesta
+        res.setHeader('Date', new Date().toUTCString()); //Fecha de la respuesta
+
+
         // Devuelve los resultados en un json
-        return res.json(results);
+        return res.json(results[0]);
     });
 });
 
